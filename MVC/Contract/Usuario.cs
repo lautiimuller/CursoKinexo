dca8fc046bf59799a88c6478d95e4336cd37438c
Lautaro <mullerlautaro@hotmail.com>
2018-10-10 16:43:17 -0300

    Autenticaciones/Vistas Usuarios

---
+++ MVC/Contract/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contract
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        public string Nick { get; set; }
+
+        public string Email { get; set; }
+
+        public char Contraseña { get; set; }
+
+        public bool TipoUsuario { get; set; }
+
+    }
+}
